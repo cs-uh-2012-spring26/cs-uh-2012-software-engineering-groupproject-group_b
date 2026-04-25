24435635a96d7473b5c514a5d1aa1f06b658dbaf
dr3596-cmd <dr3596@nyu.edu>
2026-04-26 01:40:50 +0300

    Resolving merge conflicts

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,26 +1,42 @@
 import pytest
 import os
-from app.db import DB
-from app import create_app
 from dotenv import load_dotenv
-import pytest
-
+from flask_jwt_extended import create_access_token
 from app import create_app
 from app.db import DB
-
-
-@pytest.fixture
-def mock_jwt(mocker):
-    """ Shared JWT mock fixture """
-    def setup(role):
-        mocker.patch(
-            "flask_jwt_extended.view_decorators.verify_jwt_in_request",
-            return_value=None,
-        )
-        mocker.patch("app.services.auth.get_jwt", return_value={"role": role})
-    return setup
 
 
 @pytest.fixture(scope="session", autouse=True)
+def app():
+    os.environ["MOCK_DB"] = "true"
+    load_dotenv(override=False)
+    app = create_app()
+    yield app
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture(scope="session")
 def runner(app):
     return app.test_cli_runner()
+
+
+@pytest.fixture
+def trainer_token(app):
+    with app.app_context():
+        return create_access_token(
+            identity="testId",
+            additional_claims={"role": "trainer"},
+        )
+
+
+@pytest.fixture
+def member_token(app):
+    with app.app_context():
+        return create_access_token(
+            identity="testId",
+            additional_claims={"role": "member"},
+        )
